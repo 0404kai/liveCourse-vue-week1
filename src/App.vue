da3d04672f4dd690461b50b267593ac6127eef9f
Darren_Hsu <bthd0404@gmail.com>
2022-12-18 23:45:21 +0800

    add init

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script>
+import HomeView from "./views/HomeView.vue";
+
+export default {
+  components: {
+    HomeView,
+  },
+};
+</script>
+
+<template>
+  <HomeView></HomeView>
+</template>
+
+<style lang="scss">
+@import "./assets/all.scss";
+
+img {
+  object-fit: contain;
+  max-width: 100%;
+}
+
+.primary-image {
+  height: 300px;
+}
+
+.images {
+  height: 150px;
+}
+</style>
